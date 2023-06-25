052b8ae6ac01912dd0bb561a1022b9829fe009d0
Timur Ramazanov <timramazov94@gmail.com>
2023-06-25 12:23:59 +0400

    feat: update

--- blockchain/contracts/Arbitrage.sol
+++ blockchain/contracts/Arbitrage.sol
@@ -2,7 +2,8 @@
 pragma solidity >= 0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+// import "hardhat/console.sol";
 
 interface IERC20Router {
   function getAmountsOut(uint256 amountIn, address[] memory path) external view returns(uint256[] memory amounts);
@@ -16,7 +17,7 @@ interface IERC20Router {
   ) external returns (uint256[] memory amounts);
 }
 
-contract Arbitrage {
+contract Arbitrage is Ownable {
   event Swaped(address[] path, uint256 amountOut);
 
   function reversePath(address[] memory path) private pure returns (address[] memory) {
@@ -33,47 +34,53 @@ contract Arbitrage {
 
   function estimateDualTrade(uint256 amountIn, address[] memory path, address router1Address, address router2Address) external view returns(uint256) {
     uint256[] memory amountsOut = IERC20Router(router1Address).getAmountsOut(amountIn, path);
-    uint256[] memory returnedAmountsOut = IERC20Router(router2Address).getAmountsIn(amountsOut[amountsOut.length - 1], path);
-    return returnedAmountsOut[0];
+    uint256[] memory returnedAmountsOut = IERC20Router(router2Address).getAmountsOut(amountsOut[amountsOut.length - 1], reversePath(path));
+    return returnedAmountsOut[returnedAmountsOut.length - 1];
   }
 
-  function trade(uint256 amountIn, address[] memory path, address router1Address, address router2Address) external payable  {
-    IERC20Router router1 = IERC20Router(router1Address);
-    IERC20Router router2 = IERC20Router(router2Address);
+  // function slippageTolerance(uint256 percent, uint256 amount) private pure returns(uint256) {
+  //   uint256 amountByPercent = (percent * amount) / 100;
+  //   return amount - amountByPercent;
+  // }
 
+  function trade(uint256 amountIn, address[] memory path, address router1Address, address router2Address) external onlyOwner payable  {
     uint deadline = block.timestamp + 300; // transaction expires in 300 seconds (5 minutes)
 
     uint256[] memory amounts1Out = IERC20Router(router1Address).getAmountsOut(amountIn, path);
     uint256 expectedAmount1Out = amounts1Out[amounts1Out.length - 1];
 
-    IERC20 inputToken = IERC20(path[0]);
-    // If allowance of input token less than amountIn, add allowance to router1
-    if (inputToken.allowance(msg.sender, router1Address) < amountIn) {
-      // delegatecall does not suit because it changes state of caller contract not target
-      // (bool success,) = path[0].delegatecall(abi.encodeWithSignature("approve(address, uint256)", router1Address, amountIn));
-      bool success = inputToken.approve(router1Address, amountIn);
-      console.log(success);
-    }
-    console.log(inputToken.allowance(msg.sender, router1Address));
+    IERC20(path[0]).approve(router1Address, amountIn);
+
     // Swap initial token for output token
-    uint256[] memory swappedAmountsOut = router1.swapExactTokensForTokens(amountIn, expectedAmount1Out, path, msg.sender, deadline);
+    uint256[] memory swappedAmountsOut = IERC20Router(router1Address).swapExactTokensForTokens(amountIn, expectedAmount1Out, path, address(this), deadline);
     uint256 swappedAmountOut = swappedAmountsOut[swappedAmountsOut.length - 1];
     emit Swaped(path, swappedAmountOut);
 
     address[] memory reversedPath = reversePath(path);
-    uint256[] memory amountsIn = IERC20Router(router2Address).getAmountsIn(swappedAmountOut, path);
+    uint256[] memory amountsIn = IERC20Router(router2Address).getAmountsOut(swappedAmountOut, reversedPath);
     uint256 expectedAmountIn = amountsIn[0];
 
-    // If allowance of output token less than swappedAmountOut, add allowance to router2
-    if (IERC20(reversedPath[0]).allowance(msg.sender, router2Address) < swappedAmountOut) {
-      IERC20(reversedPath[0]).approve(router2Address, swappedAmountOut);
-    }
+    IERC20(reversedPath[0]).approve(router2Address, swappedAmountOut);
 
     // Swap output token for initial token back
-    uint256[] memory finalAmountsOut = router2.swapExactTokensForTokens(swappedAmountOut, expectedAmountIn, reversedPath, msg.sender, deadline);
+    uint256[] memory finalAmountsOut = IERC20Router(router2Address).swapExactTokensForTokens(swappedAmountOut, expectedAmountIn, reversedPath, address(this), deadline);
     uint256 finalAmountOut = finalAmountsOut[finalAmountsOut.length - 1];
-    require(finalAmountOut > swappedAmountOut, 'No profit');
+
+    require(finalAmountOut > amountIn, 'No profit');
     emit Swaped(reversedPath, finalAmountsOut[finalAmountsOut.length - 1]);
+  }
+
+  function getAssetBalance(address _tokenAddress) external view returns(uint256) {
+    return IERC20(_tokenAddress).balanceOf(address(this));
+  }
+
+  function recoverBNB() external onlyOwner payable {
+    payable(msg.sender).transfer(address(this).balance);
+  }
+
+  function recoverAsset(address _tokenAddress) external onlyOwner payable {
+    IERC20 token = IERC20(_tokenAddress);
+    token.transfer(msg.sender, token.balanceOf(address(this)));
   }
 
   receive() external payable {}
--- blockchain/helpers.ts
+++ blockchain/helpers.ts
@@ -2,7 +2,7 @@ import { BigNumberish, ethers as TEthers } from "ethers"
 import { ethers } from "hardhat"
 import biswapRouterAbi from './abi/biswapRouter.abi.json'
 import wbnbAbi from './abi/wbnb.abi.json'
-import { formatUnits, parseUnits } from "./utils"
+import { parseUnits } from "./utils"
 import { PANCAKESWAP_ROUTER_ADDRESS } from "./constants"
 
 export const approve = async (contractAddress: string, contractAbi: TEthers.ContractInterface, signer: TEthers.Signer, spender: string, value: BigNumberish) => {
@@ -12,20 +12,18 @@ export const approve = async (contractAddress: string, contractAbi: TEthers.Cont
   return receipt
 }
 
-export const topupAssetToSigner = async (signer: TEthers.Signer) => {
-  const biswapRouter = new ethers.Contract(PANCAKESWAP_ROUTER_ADDRESS, biswapRouterAbi, signer)
+export const topupUsdt = async (signer: TEthers.Signer, recipient: string) => {
+  const router = new ethers.Contract(PANCAKESWAP_ROUTER_ADDRESS, biswapRouterAbi, signer)
   await approve('0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', wbnbAbi, signer, PANCAKESWAP_ROUTER_ADDRESS, 5)
   const wbnbToUsdtPath = ['0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', '0x55d398326f99059fF775485246999027B3197955']
-  const amountsOut = await biswapRouter.functions.getAmountsOut(parseUnits(2), wbnbToUsdtPath)
-  const latestBlock = await signer.provider?.getBlock('latest')
-  if (latestBlock) {
-    const deadline = latestBlock.timestamp + 3000
-    await biswapRouter.functions.swapExactTokensForTokens(
-      parseUnits(2),
-      amountsOut[0][1].toString(),
-      wbnbToUsdtPath,
-      signer.getAddress(),
-      deadline
-    )
-  }
+  const amountsOut = await router.functions.getAmountsOut(parseUnits(2), wbnbToUsdtPath)
+  const deadline = Math.floor((Date.now() + 3000) / 1000)
+
+  await router.functions.swapExactTokensForTokens(
+    parseUnits(2),
+    amountsOut[0][1].toString(),
+    wbnbToUsdtPath,
+    recipient,
+    deadline
+  )
 }
